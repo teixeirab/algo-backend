60bbc819c21fb3a7ff9860232fc275c37ca43e18
kata <kata.choi@gmail.com>
2017-04-24 08:16:50 +0800

    rename loadsData

--- app/routes/series.js
+++ app/routes/series.js
@@ -5,7 +5,7 @@ module.exports = function(app, UserController, SeriesController) {
   app.route("/api/panel/performance/:seriesNumber").get(UserController.internalAuth, SeriesController.getHistoryPerformance)
   app.route("/api/panel/risk/:seriesNumber").get(UserController.internalAuth, SeriesController.getRiskData)
   app.route("/api/panel/facts/:seriesNumber").get(UserController.internalAuth, SeriesController.getFacts)
-  app.route("/api/panel/loans/:seriesNumber").get(UserController.internalAuth, SeriesController.getLoanInfo)
+  app.route("/api/panel/loansData/:seriesNumber").get(UserController.internalAuth, SeriesController.getLoanInfo)
 
   return this
 }
