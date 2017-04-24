d9d2d78e4f3149a4dac00268c12eef30270e97e8
katat <kata.choi@gmail.com>
2017-04-25 06:28:58 +0800

    /invoices api

--- app/routes/series.js
+++ app/routes/series.js
@@ -6,6 +6,7 @@ module.exports = function(app, UserController, SeriesController) {
   app.route("/api/panel/risk/:seriesNumber").get(UserController.internalAuth, SeriesController.getRiskData)
   app.route("/api/panel/facts/:seriesNumber").get(UserController.internalAuth, SeriesController.getFacts)
   app.route("/api/panel/loansData/:seriesNumber").get(UserController.internalAuth, SeriesController.getLoanInfo)
+  app.route("/api/panel/invoices/:company").get(UserController.internalAuth, SeriesController.getInvoicesByCompany)
 
   return this
 }
