9b5135e70886ef92117ec1be07bbb336631b9f0f
katat <kata.choi@gmail.com>
2017-04-25 14:53:25 +0800

    fix pk for txn list

--- app/models/qb_transaction_list.js
+++ app/models/qb_transaction_list.js
@@ -2,8 +2,7 @@ const moment = require('moment')
 module.exports = function(FlexFundsDB, Sequelize) {
   let model = FlexFundsDB.define('qb_transaction_list', {
     qb_account: {
-      type: Sequelize.STRING,
-      allowNull: false
+      type: Sequelize.STRING
     },
     doc_num: {
       type: Sequelize.STRING,
