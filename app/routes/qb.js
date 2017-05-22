34b1304c07c9557df12d3eb9a282810b377cad39
kata <kata.choi@gmail.com>
2017-05-22 15:58:36 +0800

    fix interest invoice route

--- app/routes/qb.js
+++ app/routes/qb.js
@@ -4,7 +4,7 @@ module.exports = function(app, UserController, QBController) {
   app.route("/api/panel/qb/customer").post(UserController.internalAuth, QBController.createCustomer)
   app.route("/api/panel/qb/maintenance-invoice/:seriesNumber").post(UserController.internalAuth, QBController.generateMaintenanceInvoice)
   app.route("/api/panel/qb/setup-invoice").post(UserController.internalAuth, QBController.generateSetUpInvoice)
-  app.route("/api/panel/qb/interest-invoice/:seriesNumber").post(UserController.internalAuth, QBController.generateInterestInvoice)
+  app.route("/api/panel/qb/interest-invoice/:seriesNumber").post(UserController.auth, QBController.generateInterestInvoice)
   // app.route("/api/panel/qb/legal-invoice").post(UserController.internalAuth, QBController.generateLegalInvoice)
 
   return this
