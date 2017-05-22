6f106a9e6cea23d7afc7542bdb494ece8eb1ccc6
kata <kata.choi@gmail.com>
2017-05-22 15:12:17 +0800

    add jenkins send invoice route

--- app/routes/jenkins.js
+++ app/routes/jenkins.js
@@ -2,6 +2,7 @@
 
 module.exports = function(app, UserController, JenkinsController) {
   app.route("/api/jenkins/maintenance-fees").post(UserController.auth, JenkinsController.calcMaintenanceFees)
+  app.route("/api/jenkins/send-maintenance-fees").post(UserController.auth, JenkinsController.sendMaintenanceFeesInvoices)
 
   return this
 }
